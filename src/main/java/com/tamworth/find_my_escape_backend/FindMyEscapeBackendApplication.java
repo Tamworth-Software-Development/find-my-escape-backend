d1e10f45cbdb6be775ea942e76e9efa99e81a024
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2025-01-13 09:42:18 +0000

    Start of project

---
+++ src/main/java/com/tamworth/find_my_escape_backend/FindMyEscapeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tamworth.find_my_escape_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyEscapeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyEscapeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamworth/find_my_escape_backend/FindMyEscapeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamworth.find_my_escape_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyEscapeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
