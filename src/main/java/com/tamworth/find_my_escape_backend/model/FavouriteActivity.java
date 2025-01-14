e2d033daacbeb24ee7303c75b909d46f4729903b
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-14 10:36:39 +0000

    feat:created all the Favourite Activity entities within the FavouriteActivity class

--- src/main/java/com/tamworth/find_my_escape_backend/model/FavouriteActivity.java
+++ src/main/java/com/tamworth/find_my_escape_backend/model/FavouriteActivity.java
@@ -1,4 +1,32 @@
 package com.tamworth.find_my_escape_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "favourite_activity")
 public class FavouriteActivity {
+    @Id
+    @NonNull
+    @Column
+    private String activityId;
+
+    @Column
+    private String activityName;
+
+    @Column
+    private String activityType;
+
+    @Column
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinTable(
+            name= "UserFavouriteActivity",
+            joinColumns = @JoinColumn(name = "activityId")
+    )
+    private User FavActivityUser;
+
 }
