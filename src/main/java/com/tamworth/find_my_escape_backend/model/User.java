5ab1e2564f945f8898df3e236fe5f10b56de6adc
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-14 10:06:37 +0000

    feat:created all the user entities within the user class

--- src/main/java/com/tamworth/find_my_escape_backend/model/User.java
+++ src/main/java/com/tamworth/find_my_escape_backend/model/User.java
@@ -1,4 +1,50 @@
 package com.tamworth.find_my_escape_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
 public class User {
+    @Id
+    @NonNull
+    @Column
+    private String userId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String emailAddress;
+
+    @Column
+    private String current_Search;
+
+    @Column
+    @OneToMany(mappedBy = "FavLocationUser")
+    @JoinTable(
+            name= "UserFavouriteLocation",
+            joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "locationId"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"userId", "locationId"})
+    )
+    private Set<FavouriteLocation> favouriteLocations = new HashSet<>();
+
+    @Column
+    @OneToMany(mappedBy = "FavActivityUser")
+    @JoinTable(
+            name= "UserFavouriteLocation",
+            joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "activityId"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"userId", "activityId"})
+    )
+    private Set<FavouriteActivity> favouriteActivities = new HashSet<>();
+
 }
