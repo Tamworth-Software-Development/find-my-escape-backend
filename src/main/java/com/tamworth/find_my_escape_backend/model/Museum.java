ede441fb015d198b2ca173e8799191e5d74889f8
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-14 14:17:48 +0000

    feat:added the Musuem class (which i made a subclass of the Activity class) as well as adding its attributes to it

---
+++ src/main/java/com/tamworth/find_my_escape_backend/model/Museum.java
@@ -0,0 +1,9 @@
+package com.tamworth.find_my_escape_backend.model;
+
+public class Museum extends Activity{
+    private Double fee;
+
+    private String website;
+
+    private String openingHours;
+}
