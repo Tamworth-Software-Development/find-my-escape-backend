8820fb4a05a9825c3db37d687ee4bdba1b34d637
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2025-01-15 12:05:41 +0000

    feat: add LocationController file

---
+++ src/main/java/com/tamworth/find_my_escape_backend/controller/LocationController.java
@@ -0,0 +1,9 @@
+package com.tamworth.find_my_escape_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/location")
+public class LocationController {
+}
