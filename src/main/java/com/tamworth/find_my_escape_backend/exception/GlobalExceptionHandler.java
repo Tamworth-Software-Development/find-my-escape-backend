2d7073d95a1c015471bb0f275229f67078b316c8
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-16 15:25:33 +0000

    feat:Added the GlobalExceptionHandler class
    this class is to be used to hold al of the responses that are to be given by any exceptions found in the program and corextly handle the errors

---
+++ src/main/java/com/tamworth/find_my_escape_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.tamworth.find_my_escape_backend.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
