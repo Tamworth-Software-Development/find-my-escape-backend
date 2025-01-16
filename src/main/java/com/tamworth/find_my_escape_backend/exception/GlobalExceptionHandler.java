e0e0f004c1be161f00573bbc3110cd8fe7bad836
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-16 15:42:15 +0000

    feat: added the exception logic for ResourceNotFoundException within the GlobalExceptionHandler class

--- src/main/java/com/tamworth/find_my_escape_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tamworth/find_my_escape_backend/exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.tamworth.find_my_escape_backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
