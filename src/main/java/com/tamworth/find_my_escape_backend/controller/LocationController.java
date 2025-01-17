6924e4208fa7258e763ea37a9579b50660c023d0
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2025-01-17 10:16:45 +0000

    feat: add implementation for getLocationInformation end point

--- src/main/java/com/tamworth/find_my_escape_backend/controller/LocationController.java
+++ src/main/java/com/tamworth/find_my_escape_backend/controller/LocationController.java
@@ -2,7 +2,9 @@ package com.tamworth.find_my_escape_backend.controller;
 
 import com.tamworth.find_my_escape_backend.model.Activity;
 import com.tamworth.find_my_escape_backend.model.Location;
+import com.tamworth.find_my_escape_backend.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.http.HttpResponse;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/v1/location")
@@ -23,7 +28,10 @@ public class LocationController {
 
     @GetMapping("/information")
     public ResponseEntity<Location> getLocationInformation(@RequestBody String locationName) {
-        return null;
+        Location requestedLocation = locationService.findLocationInformation(locationName);
+        HttpStatus resultingStatus = requestedLocation == null ? NOT_FOUND:OK;
+
+        return new ResponseEntity<>(requestedLocation, resultingStatus);
     }
 
     @GetMapping("/activities")
